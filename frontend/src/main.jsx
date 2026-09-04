c7362301c69bc3fbc232aa1371772f6b7244d578
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-09-04 15:22:54 +0530

    feat: implement routing with react-router and add layout components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import {createRoot} from 'react-dom/client'
+
+import {createBrowserRouter} from 'react-router'
+import {RouterProvider} from 'react-router/dom'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './index.css'
 
 import App from './App.jsx'
+import EmptyLayout from './layouts/EmptyLayout.jsx'
+import MainLayout from './layouts/MainLayout.jsx'
 
-
+const router = createBrowserRouter([
+  {
+    Component: EmptyLayout,
+    children: [
+      {Component: MainLayout, children: [{index: true, Component: App}]},
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )

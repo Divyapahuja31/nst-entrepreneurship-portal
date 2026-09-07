d5e9d334ba1c296402cd17dd08fcdaf64e7e5c6d
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-09-07 19:22:58 +0530

    feat: implement sidebar layout with responsive drawer

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
-import {Router} from 'express'
+import { Router } from 'express'
 
-import {signUp, signIn, logout} from '../controllers/user.js'
+import { signUp, signIn, logout } from '../controllers/user.js'
 
 const router = Router()
 

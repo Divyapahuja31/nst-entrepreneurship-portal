b578036c13fcbb9ab460cce6302497d16a25e74a
kritgarg <kritg0160@gmail.com>
2026-09-08 16:46:29 +0530

    feat: implement Google signup flow with profile completion page and updated user schema

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,12 +6,16 @@ import {
   signOut,
   googleAuthCallback,
   googleAuth,
+  completeGoogleSignup,
+  getGoogleSignupOptions,
 } from '../controllers/user.js'
 
 const router = Router()
 
 router.get('/google', googleAuth)
 router.get('/google/callback', googleAuthCallback)
+router.post('/google/complete-signup', completeGoogleSignup)
+router.get('/google/signup-options', getGoogleSignupOptions)
 
 router.post('/signup', signUp)
 router.post('/signin', signIn)
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import { Form, useActionData } from 'react-router'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import { Link } from 'react-router'
+import { Typography } from '@mui/material';
 
 import '../style/auth.css'
 
@@ -10,11 +12,7 @@ function SignIn() {
   return (
     <div className="container">
       <div className="box">
-        <h1>Sign In</h1>
-        <h3>
-          Access is restricted, Student must be on the approval list; faculty
-          are reviewed manually
-        </h3>
+        <h1 style={{marginBottom:"10px"}}>Sign In</h1>
         <p> {action && action.error}</p>
 
         <Form method="post">
@@ -33,9 +31,25 @@ function SignIn() {
           />
 
           <Button fullWidth variant="contained" type="submit" sx={{ mt: 2 }}>
-            Contained
+            SignIn
           </Button>
+          <div style={{ margin: '5px', textAlign: 'center' }}>
+          OR
+        </div>
+
+        <Button
+          fullWidth
+          variant="outlined"
+          type="button"
+          onClick={() => {
+            window.location.href = '/api/auth/google'
+          }}
+        >
+          Continue with Google
+        </Button>
         </Form>
+
+        <Typography> Don't have account ? <Link to="/signup">SignUp</Link></Typography>
       </div>
     </div>
   )

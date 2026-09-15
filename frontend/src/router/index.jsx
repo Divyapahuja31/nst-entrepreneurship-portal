687b0107d79a597d733fb35d04676da40b6a4bc0
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2026-09-15 11:52:27 +0530

    reorganize routes to remove unnecesary nesting

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router'
 import EmptyLayout from '../layouts/EmptyLayout.jsx'
 import MainLayout from '../layouts/MainLayout.jsx'
+
 import Dashboard from '../pages/Dashboard.jsx'
 import Kpis from '../pages/Kpis.jsx'
 import SignIn from '../pages/SignIn.jsx'
@@ -29,74 +30,92 @@ import Portfolio from '../pages/admin/Portfolio.jsx'
 import Profile from '../pages/admin/Profile.jsx'
 import CompleteSignup from '../pages/CompleteSignup.jsx'
 import { Proposal } from '../pages/student/Proposal.jsx'
-import {
-  proposalLoader,
-  proposalAction,
-} from '../api/proposal.js'
+import { proposalLoader, proposalAction } from '../api/proposal.js'
 
 export const router = createBrowserRouter([
+  {
+    Component: MainLayout,
+    path: '/',
+    children: [
+      {
+        path: '/',
+        index: true,
+        Component: Dashboard,
+      },
+      {
+        path: '/kpis',
+        Component: Kpis,
+        loader: kpisLoader,
+        action: kpisAction,
+      },
+      {
+        path: '/proposal',
+        Component: Proposal,
+        loader: proposalLoader,
+        action: proposalAction,
+      },
+      {
+        path: '/methodology',
+        Component: Methodology,
+      },
+      {
+        path: '/profile/:userid',
+        Component: Profile,
+        loader: biWeeklyLoader,
+        action: profileAction,
+      },
+    ],
+  },
+
   {
     Component: EmptyLayout,
     children: [
       {
-        Component: MainLayout,
-        loader: portfolioLoader,
-        children: [
-          { index: true, Component: Dashboard },
-          {
-            path: 'kpis',
-            Component: Kpis,
-            loader: kpisLoader,
-            action: kpisAction,
-          },
-          { path: 'proposal', Component: Proposal, loader: proposalLoader, action: proposalAction, },
-          {
-            path: 'profile/:userid',
-            Component: Profile,
-            loader: biWeeklyLoader,
-            action: profileAction,
-          },
-          {
-            path: 'admin',
-            children: [
-              { index: true, Component: Admin, loader: foundersCount },
-              {
-                path: 'portfolio',
-                Component: Portfolio,
-                loader: foundersLoader,
-                action: portfolioAction,
-              },
-              {
-                path: 'founders/new',
-                Component: AddFounder,
-                loader: addFounderLoader,
-                action: addFounderAction,
-              },
-              { path: 'methodology', Component: Methodology },
-              {
-                path: 'profile/:userid',
-                Component: Profile,
-                loader: biWeeklyLoader,
-                action: profileAction,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: 'signin',
+        path: '/signin',
         Component: SignIn,
         action: credentialsAction('/auth/signin', 'Invalid credentials'),
       },
       {
-        path: 'signup',
+        path: '/signup',
         Component: SignUp,
         action: credentialsAction('/auth/signup', 'Something went wrong'),
       },
-      { path: 'signout', action: signoutAction },
+      { path: '/signout', action: signoutAction },
       {
-        path: 'complete-signup',
+        path: '/complete-signup',
         Component: CompleteSignup,
+      },
+    ],
+  },
+
+  {
+    path: '/admin',
+    Component: MainLayout,
+    children: [
+      {
+        path: '/admin',
+        index: true,
+        Component: Admin,
+        loader: foundersCount,
+      },
+      {
+        path: '/admin/portfolio',
+        Component: Portfolio,
+        loader: foundersLoader,
+        action: portfolioAction,
+      },
+      {
+        path: '/admin/founders/new',
+        Component: AddFounder,
+        loader: addFounderLoader,
+        action: addFounderAction,
+      },
+
+      {
+        path: '/admin/profile/:userid',
+        Component: Profile,
+        loader: biWeeklyLoader,
+        action: profileAction,
       },
     ],
   },

555b21c8f8a4b571c94ef48c3b7d4fefbb8e4cd1
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-09-16 16:43:14 +0530

    feat: implement tabbed navigation for venture management

--- frontend/src/pages/admin/Venture.jsx
+++ frontend/src/pages/admin/Venture.jsx
@@ -1,21 +1,76 @@
 import { useLoaderData } from 'react-router'
 import React from 'react'
 import CustomizedTable from '../../components/Table'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import Box from '@mui/material/Box'
+import PropTypes from 'prop-types'
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      tabIndex={0}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  )
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  }
+}
 
 const columnsName = ['name', 'campus', 'stage', 'industry']
 
 function Venture() {
   const data = useLoaderData()
   const [selectedRows, setSelectedRows] = React.useState([])
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
   return (
     <div>
-      <CustomizedTable
-        columnNames={columnsName}
-        data={data}
-        selectedRows={selectedRows}
-        setSelectedRows={setSelectedRows}
-        targetRoute="/admin/venture"
-      />
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Venture" {...a11yProps(0)} />
+          <Tab label="Proposal" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <CustomizedTable
+          columnNames={columnsName}
+          data={data}
+          selectedRows={selectedRows}
+          setSelectedRows={setSelectedRows}
+          targetRoute="/admin/venture"
+        />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
     </div>
   )
 }
